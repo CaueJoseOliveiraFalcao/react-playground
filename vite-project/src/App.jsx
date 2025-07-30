454d86ac1bf56a0581c10c57086a7395afb9991f
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2025-07-30 12:00:17 -0300

    gd

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import Header from './Header'
 import InputwText from './assets/InputwText'
+import {createBrowserRouter ,  createRoutesFromElements , RouterProvider , Route} from 'react-router-dom'
+import Home from './pages/Home';
+
+const browserRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' >
+    <Route index element={<Home/>}/>
+  </Route>
+));
+
 function App() {
 
   return (
     <>
-    <Header hideMenu={true} />
-    <Header hideMenu={false} />
-    <InputwText/>
+    <RouterProvider router={browserRouter} />
     </>
   )
 }
